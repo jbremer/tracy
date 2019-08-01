cc009b8706d39ced257003845602246d6bba0700
Jurriaan Bremer <jbr@hatching.io>
2019-08-01 14:01:20 +0200

    support for current debian unstable

--- src/zipjail/README.rst
+++ src/zipjail/README.rst
@@ -21,7 +21,7 @@ directory to which file writes should be restricted.
 .. code-block:: bash
 
     $ zipjail
-    zipjail 0.5 - safe unpacking of potentially unsafe archives.
+    zipjail 0.5.1 - safe unpacking of potentially unsafe archives.
     Copyright (C) 2016-2018, Jurriaan Bremer <jbr@hatching.io>.
     Copyright (C) 2018-2019, Hatching B.V.
     Based on Tracy by Merlijn Wajer and Bas Weelinck.
--- src/zipjail/zipjail.c
+++ src/zipjail/zipjail.c
@@ -49,7 +49,8 @@ static const char *g_syscall_allowed[] = {
     "read", "lseek", "stat", "fstat", "umask", "lstat", "utimensat",
     "exit_group", "fchmod", "utime", "getdents", "chmod", "munmap", "time",
     "rt_sigaction", "brk", "fcntl", "access", "getcwd", "chdir", "select",
-    "newfstatat", "fstat64", "_llseek", "gettimeofday", "stat64", NULL,
+    "newfstatat", "fstat64", "_llseek", "gettimeofday", "stat64",
+    "getdents64", NULL,
 };
 
 static const char *g_openat_allowed[] = {
@@ -604,7 +605,7 @@ int main(int argc, char *argv[])
 {
     if(argc < 4) {
         fprintf(stderr,
-            "zipjail 0.5 - safe unpacking of potentially unsafe archives.\n"
+            "zipjail 0.5.1 - safe unpacking of potentially unsafe archives.\n"
             "Copyright (C) 2016-2018, Jurriaan Bremer <jbr@hatching.io>.\n"
             "Copyright (C) 2018-2019, Hatching B.V.\n"
             "Based on Tracy by Merlijn Wajer and Bas Weelinck.\n"
